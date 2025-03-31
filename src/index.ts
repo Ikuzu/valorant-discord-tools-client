498afb6dc5bfc8b4da3d31883ebcd653133d151c
Ikuzu <yuzuki0905main@gmail.com>
2025-04-01 01:41:27 +0900

    コメント削除

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,11 @@ import { getUserIdViaOAuth } from './oauth-server.js'
 
 const riotManager = new RiotManager()
 
-// OAuth2で人間のユーザーIDを取得
 const userId = await getUserIdViaOAuth()
 console.log('[OAUTH USER ID]', userId)
 
 await riotManager.start()
 const xmppMitm = await riotManager.getXmppMitm()
 if (xmppMitm) {
-  await startLogReadLoop(userId, xmppMitm) // 👈 userIdを渡す！
+  await startLogReadLoop(userId, xmppMitm)
 }
